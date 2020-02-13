784a3a7409000a62f4172cc0d3116c4cae22e95e
tranquangchau <quangchau.tran@amaris.com>
2020-02-13 16:19:32 +0700

    Update RecipeController.php
    test method

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipeController extends AbstractController
 {
     /**
-     * @Route("/recipe", name="recipe")
+     * @Route("/recipe", name="recipe", methods={"GET"})
      */
     public function index()
     {
